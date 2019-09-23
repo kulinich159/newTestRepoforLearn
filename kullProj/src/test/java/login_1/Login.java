704f9fc3a01843b8c42dfa3f000b7f3ab1083f94
o.kulinich <kulinich159@gmail.com>
2019-09-23 19:17:18 +0300

    add comment

--- kullProj/src/test/java/login_1/Login.java
+++ kullProj/src/test/java/login_1/Login.java
@@ -1,7 +1,22 @@
 package login_1;
 
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
 public class Login {
 
+    @Test
+    public void validLogIn(){
 
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+        WebDriver webDriver = new ChromeDriver();
+
+    }
 
 }
