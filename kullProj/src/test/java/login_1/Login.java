f55e28a5ebbd6793a877f7b7336cf5e858a46303
o.kulinich <kulinich159@gmail.com>
2019-09-25 13:09:47 +0300

    add comment

--- kullProj/src/test/java/login_1/Login.java
+++ kullProj/src/test/java/login_1/Login.java
@@ -1,11 +1,14 @@
 package login_1;
 
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 public class Login {
 
@@ -16,6 +19,19 @@ public class Login {
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
         WebDriver webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("https://stage-board.advendor.net/");
+        System.out.println("Site was opened");
+        webDriver.findElement(By.id("email")).sendKeys("tpanfilova@codiv.io");
+        webDriver.findElement(By.id("password")).sendKeys("BqBZ7mth6Bor");
+        webDriver.findElement(By.id("loginButton")).click();
+
+        Assert.assertTrue("Left side bar was not found", webDriver.findElement(By.id("side-menu")).isDisplayed());
+        System.out.println("Valid Login was successful");
+        webDriver.quit();
 
     }
 
