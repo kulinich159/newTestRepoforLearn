5172c95c259479e8dfb0f06c5194cc3b919c8ef2
o.kulinich <kulinich159@gmail.com>
2019-09-25 19:30:49 +0300

    Pages for first test with page object

---
+++ kullProj/src/main/java/pages1/ParentPages1.java
@@ -0,0 +1,12 @@
+package pages1;
+
+import org.openqa.selenium.WebDriver;
+
+public class ParentPages1 {
+
+    WebDriver webDriver;
+
+    public ParentPages1(WebDriver webDriver) { //ALt+Insert Constructor
+        this.webDriver = webDriver;
+    }
+}
