848944f6e1cd9b80d96f11cad20ba556a0c2372b
o.kulinich <kulinich159@gmail.com>
2019-09-26 17:58:50 +0300

    Refactoring first test

--- kullProj/src/main/java/pages1/HomePage1.java
+++ kullProj/src/main/java/pages1/HomePage1.java
@@ -2,6 +2,8 @@ package pages1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage1 extends ParentPages1{
 
@@ -9,6 +11,8 @@ public class HomePage1 extends ParentPages1{
         super(webDriver);
     }
 
+    @FindBy(id = "side-menu")
+    private WebElement leftSideBarMenu;
 
     public boolean isLeftSideBarIsPresent() {
 
--- kullProj/src/main/java/pages1/ParentPages1.java
+++ kullProj/src/main/java/pages1/ParentPages1.java
@@ -1,6 +1,7 @@
 package pages1;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class ParentPages1 {
 
@@ -8,5 +9,6 @@ public class ParentPages1 {
 
     public ParentPages1(WebDriver webDriver) { //ALt+Insert Constructor
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
     }
 }
